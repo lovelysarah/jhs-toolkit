d8ad12c1b4c363d0e54f885429bb1ec017f88f1a
Sarah L. R <robichaudsarah@outlook.com>
2023-06-21 14:01:37 -0300

    :fire: Removed unused routes

--- app/routes/_app._guidelines.guidelines._index.tsx
+++
@@ -1,9 +0,0 @@
-import { Link, Outlet } from "@remix-run/react";
-
-export default function Guidelines() {
-    return (
-        <>
-            <Link to="/guidelines/lawn-mower">Lawnmower</Link>
-        </>
-    );
-}
